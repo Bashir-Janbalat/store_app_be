e71905bcc3115c7cb429a649c1fda731d18642e6
Bashir Janbalat <bashirjanbalat@gmail.com>
2025-07-10 11:17:59 +0200

    Add logging for CORS allowed origins in WebConfig

--- src/main/java/org/store/app/security/config/WebConfig.java
+++ src/main/java/org/store/app/security/config/WebConfig.java
@@ -1,15 +1,23 @@
 package org.store.app.security.config;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${cors_allowed_origin}")
     private String corsAllowedOrigin;
+
+    @PostConstruct
+    public void init() {
+        log.info("Allowed Origins = {}", corsAllowedOrigin);
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
